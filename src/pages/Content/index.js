1c52b6375d892139bbf3452e8ee0c080f1df26fc
James Lowe <james.lowe@fixdapp.com>
2021-03-28 22:46:03 -0400

    fixed blacklist

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -11,27 +11,33 @@ shadownRoot.append(content);
 container.append(shadownRoot);
 
 async function isSiteBlacklisted(url) {
-  let server = 'https://server.rexfriends.com';
-  const response = await fetch(server + '/api/get-blacklist-sites');
-  const json = await response.json();
-  for (var i = 0; i < json.list.length; i++) {
-    if (url.includes(json.list[i])) {
-      return false;
+    let server = 'https://server.rexfriends.com';
+    const response = await fetch(server + '/api/get-blacklist-sites');
+    const json = await response.json();
+    const index = getPosition(url, '/', 3) + 5;
+    console.log(index);
+    for (var i = 0; i < json.list.length; i++) {
+        if (url.substring(0, index).includes(json.list[i])) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
+}
+
+function getPosition(string, subString, index) {
+    return string.split(subString, index).join(subString).length;
 }
 
 chrome.storage.local.get(['showInjection'], (res) => {
-  if (res.showInjection === true) {
-    isSiteBlacklisted(window.location.toString()).then((res) => {
-      // console.log('Rex Allowed?', res);
-      if (res === true) {
-        render(
-          <Content />,
-          window.document.querySelector('#rex-content-injection')
-        );
-      }
-    });
-  }
+    if (res.showInjection === true) {
+        isSiteBlacklisted(window.location.toString()).then((res) => {
+            // console.log('Rex Allowed?', res);
+            if (res === true) {
+                render( <
+                    Content / > ,
+                    window.document.querySelector('#rex-content-injection')
+                );
+            }
+        });
+    }
 });
